9a537a64b0367c5740723e63726fb2ee32efe348
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2022-08-09 17:01:44 +0430

    first complete design added

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,16 @@
-'use strict';
+"use strict";
+const form_card = document.getElementById("demo");
+const result_card = document.getElementById("result_card");
+function getValue() {
+  const selected = document.querySelector('input[name="rate"]:checked').value;
+  const result = document.getElementById("result");
+
+  form_card.style.display = "none";
+  result.innerHTML = selected;
+  result_card.style.display = "block";
+}
+
+function resetStyle() {
+  form_card.style.display = "block";
+  result_card.style.display = "none";
+}
